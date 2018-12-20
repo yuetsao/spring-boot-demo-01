231ec7239052fb11ddee9615807408023e409311
yuetsao <foxmail.com>
2018-12-20 09:58:31 +0800

    启动器的位置

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动器位置：可以和controller同一个包下，或者位于controller的上级包，但是不能放到平级，以及子包下
+ */
 @SpringBootApplication
 public class DemoApplication {
 
