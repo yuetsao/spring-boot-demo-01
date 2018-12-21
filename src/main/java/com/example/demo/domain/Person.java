26c2f5de65e74e10c608a4bca0bcf2f3d090eeaf
yuetsao <foxmail.com>
2018-12-21 17:22:24 +0800

    springboot整体配置

---
+++ src/main/java/com/example/demo/domain/Person.java
@@ -0,0 +1,94 @@
+package com.example.demo.domain;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 将配置文件中的每个属性值映射到这个javabean里面
+ * @ConfigurationProperties这个注解的作用：告诉springboot将本类中所有属性和配置文件中的绑定
+ * 只有这个组件是容器中的组件，才能提供@ConfigurationProperties
+ */
+@Component
+@ConfigurationProperties(prefix="person")
+public class Person {
+    private String lastname;
+    private Integer age;
+    private Boolean boss;
+    private Date birth;
+    private Map<String, Object> maps;
+    private List<Object> lists;
+    private Dog dog;
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getBoss() {
+        return boss;
+    }
+
+    public void setBoss(Boolean boss) {
+        this.boss = boss;
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public Map<String, Object> getMaps() {
+        return maps;
+    }
+
+    public void setMaps(Map<String, Object> maps) {
+        this.maps = maps;
+    }
+
+    public List<Object> getLists() {
+        return lists;
+    }
+
+    public void setLists(List<Object> lists) {
+        this.lists = lists;
+    }
+
+    public Dog getDog() {
+        return dog;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "lastname='" + lastname + '\'' +
+                ", age=" + age +
+                ", boss=" + boss +
+                ", birth=" + birth +
+                ", maps=" + maps +
+                ", lists=" + lists +
+                ", dog=" + dog +
+                '}';
+    }
+}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,16 +1,26 @@
 package com.example.demo;
 
+import com.example.demo.domain.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * springboot的单元测试
+ * 可以在测试期间很方便类似编码一样进行自动注入容器中的功能
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 
+	@Autowired
+	Person person;
+
 	@Test
 	public void contextLoads() {
+		System.out.println(person);
 	}
 
 }
